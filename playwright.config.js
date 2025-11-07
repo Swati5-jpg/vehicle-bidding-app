102cca28991c5d47b15e152a2c7c2f5d8228db78
Swati Sharma <swatis.sharma5@gmail.com>
2025-11-07 08:51:38 +0100

    workflow.yml

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,11 @@
-// playwright.config.js
-const { defineConfig } = require('@playwright/test');
+import { defineConfig } from '@playwright/test';
 
-module.exports = defineConfig({
-  testDir: 'tests/e2e',  // Only run tests from this folder
-  timeout: 30000,
-  retries: 1,
+export default defineConfig({
+  testDir: './tests/e2e',
+  webServer: {
+    command: 'npm start',   // <-- how to start your app
+    port: 5001,             // <-- your app port
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI,
+  }
 });
